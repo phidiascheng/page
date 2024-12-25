639e025862d2348e69a6cce4de8ce16883e7107d
phidiascheng <phidiascheng@users.noreply.github.com>
2024-12-25 23:41:26 +0800

    e

--- functions/apis.js
+++ functions/apis.js
@@ -6,10 +6,15 @@ const app = express();
 
 const router = express.Router();
 
+var fs = require('fs');
+
 router.get('/', (req, res) => {
+    fs.appendFile('mynewfile3.txt', "dsd", function (err) {
+        if (err) throw err;
+    });
     res.json({
         'hello':'hi'
-    })
+    });
 });
 router.get('/about', (req, res) => {
     res.json({
