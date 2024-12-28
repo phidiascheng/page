525f3bcf7bef554ba9d7b5510c0288634daf5177
phidiascheng <phidiascheng@users.noreply.github.com>
2024-12-28 23:01:25 +0800

    testejs

--- functions/apis copy.js
+++ functions/apis copy.js
@@ -3,10 +3,13 @@ const serverless = require('serverless-http');
 const json = require('../data.json')
 
 const app = express();
-app.set('view engine', 'ejs');
 const router = express.Router();
 
-
+router.get('/', (req, res) => {
+    res.json({
+        'hello':'hi'
+    });
+});
 router.get('/about', (req, res) => {
     res.json({
         'hello':'about'
@@ -17,8 +20,7 @@ router.get('/test', (req, res) => {
 });
 
 router.get('/index', (req, res) => {
-    var test = "dsds";
-    res.json({"hello":`${test}`});
+
 });
 
 router.get('/blogs', (req, res) => {
@@ -48,10 +50,20 @@ router.get('/bloglist', (req, res) => {
     res.json(resJason);
 });
 
+var head = `
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>`;
+
+var header = `
+    <div class="header">
+        <h2>Cheng's Blog</h2>
+    </div>`;
+
+
+
 app.use('/api/',router);
 module.exports.handler = serverless(app);
 
 
-router.get('/', (req, res) => {
-    res.render('test',{name:"cheng"});
-});
