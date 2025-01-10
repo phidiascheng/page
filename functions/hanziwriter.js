cdfd0784d2a3009fcb200082771accfda25392e3
phidiascheng <phidiascheng@users.noreply.github.com>
2025-01-11 07:59:44 +0800

    d

--- functions/hanziwriter.js
+++ functions/hanziwriter.js
@@ -6,7 +6,7 @@ function getchar(char){
 //    const a = path.join('hanzi-writer-data','我.json');
 //    const a = '阿.json';
 //    const json = require('hanzi-writer-data/'+a);
-    const a = '../../../../data/hanzi-writer-data/'+char+'.json';
+    const a = '../../../data/hanzi-writer-data/'+char+'.json';
     const json = require(a);
     return(json);
 }
