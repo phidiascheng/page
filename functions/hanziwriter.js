c8b825fe3e48ce1b28ebd0c2e7783f15735a52cc
phidiascheng <phidiascheng@users.noreply.github.com>
2025-01-10 23:06:18 +0800

    的

--- functions/hanziwriter.js
+++ functions/hanziwriter.js
@@ -1,10 +1,13 @@
 //import HanziWriter from 'hanzi-writer-data';
+const path = require('path');
 
 function getchar(char){
-    console.log(char);
-    var url ="hanzi-writer-data/"+char;
-    const data = require(url);
-    return(data);
+//    console.log(char);
+//    const a = path.join(__dirname,'../../../../','data/hanzi-writer-data',char+'.json')
+    const json = require('hanzi-writer-data/阿')
+//    const a = '../../../../data/hanzi-writer-data/'+char+'.json'
+//    const json = require(a);
+    return(json);
 }
 
 module.exports = {getchar}
