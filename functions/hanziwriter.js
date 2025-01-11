d12a01af6aeb42dcb8c5cc3f030ad360762ddc1c
phidiascheng <phidiascheng@users.noreply.github.com>
2025-01-11 08:13:32 +0800

    d

--- functions/hanziwriter.js
+++ functions/hanziwriter.js
@@ -1,11 +1,11 @@
 //import HanziWriter from 'hanzi-writer-data';
-const path = require('path');
+//const path = require('path');
 
 function getchar(char){
 //    const a = path.join(__dirname,'../../../../','data/hanzi-writer-data',char+'.json')
 //    const a = path.join('hanzi-writer-data','我.json');
 //    const a = '阿.json';
-//    const json = require('hanzi-writer-data/'+a);
+//    const json = require('hanzi-writer-data/'+a);;;;;
     const a = '../data/hanzi-writer-data/'+char+'.json';
     const json = require(a);
     return(json);
